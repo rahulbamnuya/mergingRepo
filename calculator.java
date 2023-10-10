9fd48b1e8b730286f250b8d7055c56046aad8604
rahulbamnuya <129646171+rahulbamnuya@users.noreply.github.com>
2023-10-10 23:30:28 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -9,10 +9,10 @@ class calc
 	public int add(int n1, int n2){
 		return n1+n2;
 	}
-	public int add(int n1, int n2,int n3){
+	public int mul(int n1, int n2,int n3){
 		return n1*n2*n3;
 	}
-	public int add(int n1, int n2){
+	public int mul(int n1, int n2){
 		return n1*n2;
 	}
 
